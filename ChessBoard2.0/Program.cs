df2bafe30afd2ddffb266e656be070184a5575c0
exikoz <hussein.hasnawy@gmail.com>
2025-10-18 18:07:56 +0200

    Initial commit - project setup

---
+++ ChessBoard2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessBoard2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
